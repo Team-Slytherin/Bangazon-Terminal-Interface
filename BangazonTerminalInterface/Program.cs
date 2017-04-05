8e97f10a54b32c4ca113a56d1eec2fa89b41e111
Taylor Harry <taylor.andrew.harry@gmail.com>
2017-04-04 17:02:22 -0700

    modified program to include a main menu

--- BangazonTerminalInterface/Program.cs
+++ BangazonTerminalInterface/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BangazonTerminalInterface
@@ -10,6 +11,54 @@ namespace BangazonTerminalInterface
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(57, 35);
+            string menuChoice = "0";
+            while (menuChoice != "7")
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(
+                  "*********************************************************" + "\n"
+                + "**   Welcome to Bangazon!Command Line Ordering System  **" + "\n"
+                + "*********************************************************");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(
+                  "1.Create a customer account" + "\n"
+                + "2.Choose active customer" + "\n"
+                + "3.Create a payment option" + "\n"
+                + "4.Add product to shopping cart" + "\n"
+                + "5.Complete an order" + "\n"
+                + "6.See product popularity" + "\n"
+                + "7.Leave Bangazon!" + "\n"
+                + "> ");
+                Console.ForegroundColor = ConsoleColor.White;
+                var userInput = Console.ReadKey(true).KeyChar.ToString();
+
+                switch (userInput)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        Console.WriteLine("Goodbye!");
+                        Thread.Sleep(1500);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("please select a valid menu item...");
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }
         }
     }
 }
