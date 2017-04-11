e43a54f41ca4acd89d22693de9a346dcc7009626
Debra Gordon <debra.m.gordon@gmail.com>
2017-04-10 18:29:39 -0700

    updated payment validation

--- BangazonTerminalInterface/DataValidation/PaymentValidation/AccountNumberValid.cs
+++ BangazonTerminalInterface/DataValidation/PaymentValidation/AccountNumberValid.cs
@@ -7,14 +7,12 @@ using System.Threading.Tasks;
 
 namespace BangazonTerminalInterface.DataValidation.PaymentValidation
 {
-    class AccountNumberValid
+    public class AccountNumberValid
     {
         public bool ValidatePaymentAccountNumber(string accountNumberStr)
         {
-            bool isValidVisa = Regex.IsMatch(accountNumberStr, @"^4[0-9]{12}(?:[0-9]{3})?$");
-            bool isValidMC = Regex.IsMatch(accountNumberStr, @"^[0-9]{16}$");
-
-            if (isValidVisa || isValidMC)
+            bool isValidCC = Regex.IsMatch(accountNumberStr, @"^\d{4}-\d{4}-\d{4}-\d{4}$");
+            if (isValidCC)
                 return true;
             else
                 return false;
