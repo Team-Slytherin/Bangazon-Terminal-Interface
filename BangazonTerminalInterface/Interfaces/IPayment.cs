c5f1ffbfbfe2b0f3a357afed345a635c0f98c385
Dustin Reed <dmreedy1@gmail.com>
2017-04-11 22:38:19 -0500

    cleaned up code and added IPayment interface

---
+++ BangazonTerminalInterface/Interfaces/IPayment.cs
@@ -0,0 +1,18 @@
+﻿using BangazonTerminalInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangazonTerminalInterface.Interfaces
+{
+    public interface IPayment
+    {
+
+        void RequestPayment(Customer customer);
+
+        void addPaymentToDb();
+
+    }
+}
